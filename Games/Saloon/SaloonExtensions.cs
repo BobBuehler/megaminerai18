9ddae6a0fef8df6e9b127cf3a659695722c61ee3
SMst00141T mst00141 <mst00141@rc08xcs213.managed.mst.edu>
2016-11-12 14:28:27 -0600

    Playing pianos.

---
+++ Games/Saloon/SaloonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Joueur.cs.Games.Saloon;
+
+static class SaloonExtensions
+{
+    public static Point ToPoint(this Tile tile)
+    {
+        return new Point(tile.X, tile.Y);
+    }
+
+    public static Point ToPoint(this Cowboy cowboy)
+    {
+        return cowboy.Tile.ToPoint();
+    }
+
+    public static Point ToPoint(this Furnishing furnishing)
+    {
+        return furnishing.Tile.ToPoint();
+    }
+}
